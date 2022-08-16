82752ac9aada668048709faf56c571ec7594b40b
mkmk299 <ohmk0909@gmail.com>
2022-08-16 17:28:41 +0900

    feat: add RC code

---
+++ main/demo.ino
@@ -0,0 +1,13 @@
+void setup() 
+{ 
+  pinMode(13, OUTPUT); 
+  Serial.begin(9600); 
+  } 
+ 
+void loop() 
+{ 
+  digitalWrite(13, !(digitalRead(13))); 
+    Serial.println("aaa"); 
+  delay(500); 
+   
+  }

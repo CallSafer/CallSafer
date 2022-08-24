3a5081c157a895bc89af2d267dc1cd68d4e07a0e
RayLee <wooo519@naver.com>
2022-08-24 22:23:15 +0900

    chore : clean bluetooth RC test code
    발표 준비용

--- main/RC/code.ino
+++ main/RC/code.ino
@@ -37,52 +37,65 @@ void loop()
   }
   if (val == '1')
   {
-    control.Motor(200, 1);         // 150의 속도로 자동차가 전진합니다.
-    distance = sensor.Ranging(CM); //거리를 측정하고 distance 변수에 저장합니다.
-
-    delay(mesurement_speed); //센서측정 속도를 제어하기 위한 딜레이.
-
-    //다음은 장애물이 없는 경우에 해당합니다.
-    Serial.print("No obstacle "); //장애물이 없다고 시리얼 모니터에 출력합니다.
-    Serial.println(distance);     //거리를 출력합니다.
-    Serial.print("Random ");      //"Random"을 출력합니다.
-    Serial.println(random_value); //랜덤값을 출력합니다.
-
-    random_value = random(2); //자동차가 한쪽 방향으로만 회전하는 것을 막기 위해 랜덤값을 만듭니다.
-
-    while (distance < 50)
-    { //장애물과의 거리가 30cm이하일 경우에 적용됩니다.
-
-      delay(mesurement_speed); //센서측정 속도를 제어하기 위한 딜레이.
-                               // control.Motor(0,1);//모터를 정지시킵니다.
-      distance = sensor.Ranging(CM);
-      delay(1000);
-
-      if (random_value == 0) // 랜덤값이 0인경우에 해당됩니다.
-      {
-        Serial.print("Distance ");    //"Distance "를 시리얼모니터에 출력합니다.
-        Serial.println(distance);     //거리를 출력합니다.
-        Serial.print("Random ");      //"Random"을 출력합니다.
-        Serial.println(random_value); //랜덤값을 출력합니다.
-
-        control.Motor(130, 100); //자동차가 0.4초동안 오른쪽으로 회전합니다.
-        delay(400);
-      }
-
-      else if (random_value == 1) //랜덤값이 1인경우에 해당됩니다.
-      {
-        Serial.print("Distance ");    //"Distance "를 시리얼모니터에 출력합니다.
-        Serial.println(distance);     //거리를 출력합니다.
-        Serial.print("Random ");      //"Random"을 출력합니다.
-        Serial.println(random_value); //랜덤값을 출력합니다.
-
-        control.Motor(130, -100); //자동차가 0.4초동안 왼쪽으로 회전합니다.
-        delay(400);
-      }
-    }
+    MotorCode();
   }
   else if (val == '0')
   {
     control.Motor(0, 1);
   }
 } // 블루투스 센서
+
+void MotorCode()
+{
+  control.Motor(300, 1);         // 150의 속도로 자동차가 전진합니다.
+  distance = sensor.Ranging(CM); //거리를 측정하고 distance 변수에 저장합니다.
+
+  delay(mesurement_speed); //센서측정 속도를 제어하기 위한 딜레이.
+
+  //다음은 장애물이 없는 경우에 해당합니다.
+  Serial.print("No obstacle "); //장애물이 없다고 시리얼 모니터에 출력합니다.
+  Serial.println(distance);     //거리를 출력합니다.
+  Serial.print("Random ");      //"Random"을 출력합니다.
+  Serial.println(random_value); //랜덤값을 출력합니다.
+
+  random_value = random(2); //자동차가 한쪽 방향으로만 회전하는 것을 막기 위해 랜덤값을 만듭니다.
+
+  while (distance < 50)
+  { //장애물과의 거리가 30cm이하일 경우에 적용됩니다.
+
+    delay(mesurement_speed); //센서측정 속도를 제어하기 위한 딜레이.
+                             // control.Motor(0,1);//모터를 정지시킵니다.
+    distance = sensor.Ranging(CM);
+    delay(1000);
+
+    if (random_value == 0) // 랜덤값이 0인경우에 해당됩니다.
+    {
+      MotorRight();
+    }
+
+    else if (random_value == 1) //랜덤값이 1인경우에 해당됩니다.
+    {
+      MotorLeft();
+    }
+  }
+}
+
+void MotorRight()
+{
+  Serial.print("Distance ");    //"Distance "를 시리얼모니터에 출력합니다.
+  Serial.println(distance);     //거리를 출력합니다.
+  Serial.print("Random ");      //"Random"을 출력합니다.
+  Serial.println(random_value); //랜덤값을 출력합니다.
+  control.Motor(130, 100);      //자동차가 0.4초동안 오른쪽으로 회전합니다.
+  delay(400);
+}
+
+void MotorLeft()
+{
+  Serial.print("Distance ");    //"Distance "를 시리얼모니터에 출력합니다.
+  Serial.println(distance);     //거리를 출력합니다.
+  Serial.print("Random ");      //"Random"을 출력합니다.
+  Serial.println(random_value); //랜덤값을 출력합니다.
+  control.Motor(130, -100);     //자동차가 0.4초동안 왼쪽으로 회전합니다.
+  delay(400);
+}

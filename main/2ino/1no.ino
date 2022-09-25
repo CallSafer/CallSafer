47c2d0d208f8fcba83d22c529843302b4cb19458
RayLee <wooo519@naver.com>
2022-09-25 20:53:48 +0900

    feat : 2ino test

---
+++ main/2ino/1no.ino
@@ -0,0 +1,14 @@
+void setup(){
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+}
+
+void loop(){
+  if(Serial.available()){
+   char c = (char)Serial.read();
+    if(c == '1') {
+      digitalWrite(13, HIGH);
+    } else {
+      digitalWrite(13, LOW);
+    }
+  }  
